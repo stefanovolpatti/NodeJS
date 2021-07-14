ac6f60abd24acb911fb57705acb5c92e23814499
volp99 <stefanovolpatti@yahoo.it>
2021-07-14 17:14:15 +0200

    end customizing http requestes

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,9 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=2cb72b90348af8a69619e1f652c694bc&query=Spilimbergo'
+const url = 'http://api.weatherstack.com/current?access_key=2cb72b90348af8a69619e1f652c694bc&query=Spilimbergo&units=m'
 
-request ({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request ({url: url, json: true}, (error, response) => {
+    //console.log(response.body.current)
+    console.log('In ' + response.body.location.name + 'is' response.body.current.weather_descriptions[0] + '. A ' + ' al momento ci sono ' + response.body.current.temperature + ' gradi')
 })
+
