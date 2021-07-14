504179b66bb1cee34020658b1e9a25a8b2056510
volp99 <stefanovolpatti@yahoo.it>
2021-07-14 15:45:33 +0200

    setting up http request weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('postman-request');
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=2cb72b90348af8a69619e1f652c694bc&query=Spilimbergo'
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0)
-
-console.log('Stopping')
+request ({url: url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
