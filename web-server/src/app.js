d57c0a917ba66ed20bb12683ddc46c8e9eeeefe7
stefano.volpatti@bizaway.com <steafno.volpatti@bizaway.com>
2021-07-16 17:33:29 +0200

    end build json http endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -43,12 +45,25 @@ app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
             error: 'You must provide an address!'
-        });
+        })
     }
-    res.send({
-        forecast: 'Sunny',
-        location: 'Spilimbergo',
-        address: req.query.address
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=2cb72b90348af8a69619e1f652c694bc&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error){
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined,  body.current.weather_descriptions[0] + ' in ' + body.location.name + '. There are ' + body.current.temperature + ' degree') 
+        }
+    })
+}
+
+module.exports = forecast;
