b153da66a109e89d4d817fdd32b36770f2e2d411
stefano.volpatti@bizaway.com <steafno.volpatti@bizaway.com>
2021-07-16 12:32:11 +0200

    end serving up html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,16 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectory = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    res.send('Hello express')
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help page')
-}) 
-
-app.get('/about', (req, res) => {
-    res.send('About page')
-})
+app.use(express.static(publicDirectory));
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast: 'Sunny',
+        location: 'Spilimbergo'
+    })
 })
 
 app.listen(3000, () => {
