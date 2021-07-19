7b4095e029f529e73b2f9e7413a9685c4900c8da
stefano.volpatti@bizaway.com <steafno.volpatti@bizaway.com>
2021-07-19 17:02:10 +0200

    end creating a search form

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,  body.current.weather_descriptions[0] + ' in ' + body.location.name + '. There are ' + body.current.temperature + ' degree') 
+            callback(undefined,  body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degreess out.' + ' There is a ' + body.current.precip + '% chance of rain.') 
         }
     })
 }
