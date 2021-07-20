eab2d265dd49b33c7400f2a5a60492ff52328030
stefano.volpatti@bizaway.com <steafno.volpatti@bizaway.com>
2021-07-20 17:14:04 +0200

    change the weather text

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,  body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out.' + ' There is a ' + body.current.precip + '% chance of rain 🌧') 
+            callback(undefined,  body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out but it feels like ' + body.current.feelslike + '. ' 
+            + ' There is a ' + body.current.precip + '% chance of rain 🌧  and the humidity is ' + body.current.humidity + '%.') 
         }
     })
 }
