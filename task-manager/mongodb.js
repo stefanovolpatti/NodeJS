11ffff312c4d09cd78357a944850372ad8b1e776
stefano.volpatti@bizaway.com <steafno.volpatti@bizaway.com>
2021-07-27 12:37:21 +0200

    end object id

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,61 +1,33 @@
 // CRUD    : Create, Read, Update, Delete
-
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectId()
-console.log(id.id.length)
-console.log(id.toHexString().length)
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
-        return console.log('Error connecting to database: ' + error);
+        return console.log('Unable to connect to database!')
     }
-    
-    const db = client.db(databaseName)
-   
-    // db.collection('users').insertOne ({
-    //     name: 'Vikram',
-    //     age: 25,
-    // }, (error, result) => { //callback
-    //     if (error) {
-    //         return console.log('Error inserting document into collection: ' + error);
-    //     }
-    //     console.log(result.ops);
-    // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'John',
-    //         age: '30',
-    //     }, {
-    //         name: 'Mark',
-    //         age: '28',
-    //     }
-    // ], (error, result) => {
-    //     if(error) {
-    //         return console.log('Error inserting documents into collection: ' + error);
-    //     }
-    //     console.log(result.ops);
-    // })
-    // db.collection('task').insertMany ([
-    //     {
-    //         description: 'Buy milk',
-    //         completed: true,
-    //     }, {
-    //         description: 'Learn JavaScript',
-    //         completed: false,
-    //     }, {
-    //         description: 'Learn MongoDB',
-    //         completed: false,
-    //     }
-    // ], (error, result) => {
-    //     if(error) {
-    //         return console.log('Error inserting documents into collection: ' + error);
-    //     }
-    //     console.log(result.ops);
-    //  })
+    const db = client.db(databaseName)
+    
+    db.collection('users').findOne({ _id: new ObjectId("60ffcb3a66bf552f60b22db2") }, (error, user) => {
+        if (error) {
+            return console.log('Unable to fetch')
+        }
+
+        console.log(user)
+    })
+
+    db.collection('users').find({ age: 30 }).toArray((error, users) => {
+        console.log(users)
+    })
+
+    db.collection('tasks').findOne({ _id: new ObjectId("5c0fec243ef6bdfbe1d62e2f") }, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks)
+    })
 })
