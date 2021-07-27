f1793c008d59e19d494d15ae937932b1a27958dd
stefano.volpatti@bizaway.com <steafno.volpatti@bizaway.com>
2021-07-27 11:57:02 +0200

    end inserting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,8 +12,45 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
     
     const db = client.db(databaseName)
-    db.collection('users').insertOne ({
-        name: 'Stefano',
-        age: '21',
-    })
+    // db.collection('users').insertOne ({
+    //     name: 'Stefano',
+    //     age: '21',
+    // }, (error, result) => { //callback
+    //     if (error) {
+    //         return console.log('Error inserting document into collection: ' + error);
+    //     }
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'John',
+    //         age: '30',
+    //     }, {
+    //         name: 'Mark',
+    //         age: '28',
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Error inserting documents into collection: ' + error);
+    //     }
+    //     console.log(result.ops);
+    // })
+    db.collection('task').insertMany ([
+        {
+            description: 'Buy milk',
+            completed: true,
+        }, {
+            description: 'Learn JavaScript',
+            completed: false,
+        }, {
+            description: 'Learn MongoDB',
+            completed: false,
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Error inserting documents into collection: ' + error);
+        }
+        console.log(result.ops);
+     })
 })
